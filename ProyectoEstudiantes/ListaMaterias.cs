59c165cad6686184a25a3e70b697bdce33b8f409
Salo <salo.franco2001@gmail.com>
2026-03-18 18:34:31 -0500

    Creacion de la estructura de la clase ListaMaterias

--- ProyectoEstudiantes/ListaMaterias.cs
+++ ProyectoEstudiantes/ListaMaterias.cs
@@ -10,6 +10,28 @@ namespace SistemaGestionEstudiantes
         {
             cabeza = null;
         }
+
+        public void AgregarMateria(string nombre, double nota)
+        {
+            Materia nuevaMateria = new Materia(nombre, nota);
+            NodoMateria nuevoNodo = new NodoMateria(nuevaMateria);
+
+            if (cabeza == null)
+            {
+                cabeza = nuevoNodo;
+            }
+            else
+            {
+                NodoMateria actual = cabeza;
+
+                 while (actual.siguiente != null)
+                {
+                    actual = actual.siguiente;
+                }
+
+                    actual.siguiente = nuevoNodo;
+                }
+            }
+        }
     }
-}
 
